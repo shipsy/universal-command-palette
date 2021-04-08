182ea7a74aab62583ba0a81ea67f0d9a73abb522
Pulkit Singh <pulkit.singh@shipsy.co.in>
2021-04-08 18:13:35 +0530

    exposed & bleeding :(

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,5 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+export { OptionService, Option } from "./lib/types";
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-};
+export { default as QuicklyProvider} from "./lib/components/QuicklyProvider";
+export { default as useQuickly } from "./lib/hooks/useQuickly";
+export { default as QuicklyLauncher } from './lib/components/QuicklyLauncher';
